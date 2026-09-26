4f42ea89e8ecaf4b349f768166393b6ae6c847e1
zakriakhanx <zakriakhanz2003@gmail.com>
2026-09-26 23:53:49 +0500

    update: change hover background color for navbar links for improved visibility

--- frontend/components/ui/Navbar.tsx
+++ frontend/components/ui/Navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar(): JSX.Element {
     `px-4 py-1 rounded-lg transition ${
       pathname === href
         ? "bg-brand-fg text-brand-bg"
-        : "text-brand-fg hover:shadow-[inset_0px_0px_4px_rgba(0,0,0,0.6)] hover:bg-brand-bg"
+        : "text-brand-fg hover:shadow-[inset_0px_0px_4px_rgba(0,0,0,0.6)] hover:bg-[#c9c6c6]"
     }`;
 
   return (
